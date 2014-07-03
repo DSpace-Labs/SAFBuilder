fae8c56ba653a51d7628306a36248e9402bd326c
Peter Dietz <peter@longsight.com>
2014-07-03 14:15:17 -0400

    Changed build process to use Maven
    To Rebuild: mvn clean package
    To Run: same as before, must rebuild first though.

--- recompile.sh
+++
@@ -1,2 +0,0 @@
-javac -classpath javacsv-2.0.jar:commons-io-1.4.jar:xmlwriter-2.2.jar:commons-vfs-1.0.jar:commons-logging-1.1.1.jar src/java/safbuilder/*.java -d classes
-echo "SAFBuilder has recompiled."
--- safbuilder.sh
+++ safbuilder.sh
@@ -1,1 +1,2 @@
-java -cp classes:javacsv-2.0.jar:commons-io-1.4.jar:xmlwriter-2.2.jar:commons-vfs-1.0.jar:commons-logging-1.1.1.jar safbuilder.BatchProcess $1 $2
+#!/bin/sh
+mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="$1 $2"

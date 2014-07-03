fb746723eb52c171a29112657a803c6438bbd4ee
Peter Dietz <peter@longsight.com>
2014-07-03 14:18:16 -0400

    Move code from src/java to src/main/java, as per maven standards

---
+++ src/test/java/safbuilder/SAFPackageTest.java
@@ -0,0 +1,38 @@
+package safbuilder;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SAFPackageTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SAFPackageTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( SAFPackageTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

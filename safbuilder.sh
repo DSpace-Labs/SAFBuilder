10fba309bd699e0f1591ecd46b7783f7df2c204c
Peter Dietz <peter@longsight.com>
2014-07-07 10:09:56 -0400

    Have the SAFBuilder.sh command also run mvn package, as a fallback

--- safbuilder.sh
+++ safbuilder.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+if [ ! -d "target" ]; then
+  echo "Recompiling SAFBuilder, just a moment..."
+  mvn --quiet -DskipTests=true clean package
+fi
+
+## arg1 = Path to content files directory
+## arg2 = Metadata CSV filename
 mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="$1 $2"

ded17f26048c6dc85d87b2cb7a8b8d9e8a495637
Peter Dietz <peter@longsight.com>
2014-09-05 09:19:30 -0400

    Trim whitespace from args to be compatible with maven2

--- safbuilder.sh
+++ safbuilder.sh
@@ -4,4 +4,6 @@ mvn --quiet -DskipTests=true clean package
 
 ## arg1 = Path to content files directory
 ## arg2 = Metadata CSV filename
-mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="$1 $2 $3 $4 $5 $6"
+args="$1 $2 $3 $4 $5 $6"
+argsTrim=$(echo $args | sed 's/ *$//')
+mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="$argsTrim"

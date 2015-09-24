414a4899f63ef54d16922d9d64a793dcb73e4957
Peter Dietz <peter@longsight.com>
2015-09-24 17:06:46 -0400

    Use windows friendly call prefix to call maven

--- safbuilder.bat
+++ safbuilder.bat
@@ -1,3 +1,3 @@
 @echo off
-mvn --quiet -DskipTests=true clean package
-mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="%1 %2"
+call mvn --quiet -DskipTests=true clean package
+call mvn --quiet exec:java -Dexec.mainClass="safbuilder.BatchProcess" -Dexec.args="%~1 %~2 %~3 %~4"
